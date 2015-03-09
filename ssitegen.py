7b3569ae31a95a030fa917f4d5b47a1aec26a573
Kenny Rachuonyo <krmboya@gmail.com>
2015-03-09 08:09:08 +0300

    added directory creation function

--- ssitegen.py
+++ ssitegen.py
@@ -1,3 +1,4 @@
+import os
 import argparse
 
 parser = argparse.ArgumentParser(add_help=True, description="A simple static site generator")
@@ -7,4 +8,8 @@ parser.add_argument("-i", "--initialize", action="store", default=None, dest="di
 
 args = parser.parse_args()
 
+def ensure_dir_exists(dirname, mode=0o755):
+    """Creates a directory with the given name if doesn't already exist"""
 
+    if not os.path.exists(dirname):
+        os.mkdir(dirname, mode)
