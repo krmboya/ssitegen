247839a137b29f97f42f40064469c71de2f650c9
Kenny Rachuonyo <krmboya@gmail.com>
2015-11-21 15:51:22 +0300

    can now create source directory and subdirectories

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ ssitegen [-h] [-i DIRNAME ]
 
 # DESCRIPTION
 ssitegen generates a collection of html files that can be used to serve a website from
-input files written using the markdown syntax. It uses an opinionated directory structure
+input files written in markdown. It uses an opinionated directory structure
 for both the input files and the output html files, plus other static assets like
 images and stylesheets. A website can then be served from the output directory depending
 on the preferred method of hosting, for example by pointing a webserver to its location or 
--- ssitegen.py
+++ ssitegen.py
@@ -1,17 +1,37 @@
+#!/usr/bin/env python
 import os
 import argparse
+import sys
 
 parser = argparse.ArgumentParser(add_help=True, description="A simple static site generator")
 
 parser.add_argument("-i", "--initialize", action="store", default=None, dest="dirname",
                     help="Initialize the directory structure inside DIRNAME")
 
-args = parser.parse_args()
 
-INPUT_DIRS = ('static', 'templates', 'content/entries', 'content/pages')
+SOURCE_SUBDIRS = ('static', 'templates', 'content/entries', 'content/pages')
 
 def ensure_dir_exists(dirname, mode=0o755):
     """Creates a directory with the given name if doesn't already exist"""
 
     if not os.path.exists(dirname):
         os.makedirs(dirname, mode)
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    if args.dirname:
+        source_dir = args.dirname
+        if os.path.exists(source_dir):
+            # already exists, print error and exit
+            err_msg = "Directory '{0}' already exists\n".format(source_dir)
+            sys.stderr.write(err_msg)
+        else:
+            # create directory and subdirectories
+            ensure_dir_exists(source_dir)
+            for subdir in SOURCE_SUBDIRS:
+                path = os.path.join(source_dir, subdir)
+                ensure_dir_exists(path)
+    else:
+        print "generating output"
+    
