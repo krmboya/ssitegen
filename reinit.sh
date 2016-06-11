d2c6bb4ef7dc1ef355931845ce610550cc2ec64e
Kenny Rachuonyo <krmboya@gmail.com>
2016-06-11 13:08:15 +0300

    also preserve .git and .gitignore when reinitializing site directory

--- reinit.sh
+++ reinit.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 # A wrapper around `ssitegen.py` that reinitializes the site directory
-# Preserving the `settings` file and `content` directory
-# Invoke the script with the site directory as an argument
+# Preserving the `settings` file, `content` directory, `.git` and `.gitignore`
+# where available.
+# Invoke the script with the site directory as an argument.
 # The old stuff is stored in `/tmp`
+
 
 if [ $# -ne 1 ]; then
     echo "usage: $0 <sitedirectory>"
@@ -14,3 +16,11 @@ ssitegen.py -i "$1"
 mv "$1"/content /tmp/content$$
 mv /tmp/sitebackup$$/settings "$1"/settings
 mv /tmp/sitebackup$$/content "$1"
+
+if [ -d /tmp/sitebackup$$/.git ]; then
+    mv /tmp/sitebackup$$/.git "$1"
+fi
+
+if [ -f /tmp/sitebackup$$/.gitignore ]; then
+    mv /tmp/sitebackup$$/.gitignore "$1"
+fi
